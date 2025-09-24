c86046b01ce1c22a2aea6e1c14c84ca3cdf3d8c4
AnnaLuneva <annashleeva123@gmail.com>
2025-09-25 00:04:12 +0300

    add homework1

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,7 +2,7 @@ public class Homework1 {
 
     public static void main(String[] args) {
         int b = 3;
-        int c = 8;
+        int c = 9;
         int a = 4 * (b + c - 1) / 2;
         System.out.println(a);
     }
