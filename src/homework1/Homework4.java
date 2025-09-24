7710c81949028aec5b0914744349571c6cc4df81
AnnaLuneva <annashleeva123@gmail.com>
2025-09-25 01:35:27 +0300

    add homework4

---
+++ src/homework1/Homework4.java
@@ -0,0 +1,9 @@
+package homework1;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        double n = 2.634;
+        int d = (int) n;
+        System.out.println(d);
+    }
+}
