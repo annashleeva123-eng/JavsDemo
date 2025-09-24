d0310d943226c00e6217a608d979288ad6d9c599
AnnaLuneva <annashleeva123@gmail.com>
2025-09-24 23:31:07 +0300

    Задача1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
+        int b = 3;
+        int c = 5;
+        int a = 4 * (b + c - 1) / 2;
+        System.out.println(a);
     }
 }
