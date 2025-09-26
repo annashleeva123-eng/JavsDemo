46cacb43f0c241df974e39874b51d1411bbe0fa2
AnnaLuneva <annashleeva123@gmail.com>
2025-09-26 14:42:24 +0300

    add homework5

--- src/homework1/Homework4.java
+++ src/homework1/Homework4.java
@@ -2,8 +2,8 @@ package homework1;
 
 public class Homework4 {
     public static void main(String[] args) {
-        double n = 2.634;
-        int d = (int) n;
+        double n = 2.434;
+        int d = (int) (n+0.5);
         System.out.println(d);
     }
 }
