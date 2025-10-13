6481efa59984b155d92fd3799b90a86c48a67311
AnnaLuneva <annashleeva123@gmail.com>
2025-10-13 18:47:28 +0300

    add homework2.1

--- src/homework2/Homework1.java
+++ src/homework2/Homework1.java
@@ -1,7 +1,19 @@
 package homework2;
 
+import java.util.Scanner;
+
 public class Homework1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        //запрос числа
+        System.out.print("Введите число: ");
+        int a = scanner.nextInt();
+        //Проверка четности
+        if (a % 2 == 0) {
+            System.out.println("Четное число");
+        } else {
+            System.out.println("Нечетное число");
+        }
+        scanner.close();
     }
 }
