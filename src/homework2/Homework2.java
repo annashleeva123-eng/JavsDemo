9ecb2dbf2927014efb888e2a0c8acd1c4c2d5cf2
AnnaLuneva <annashleeva123@gmail.com>
2025-10-13 19:08:04 +0300

    add homework2.2

--- src/homework2/Homework2.java
+++ src/homework2/Homework2.java
@@ -1,4 +1,21 @@
 package homework2;
 
+import java.util.Scanner;
+
 public class Homework2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //запрос температуры
+        System.out.print("Введите температуру: ");
+        int t = scanner.nextInt();
+        //Проверка условий
+        if (t > -5) {
+            System.out.println("Warm");
+        } else if (-20 >= t) {
+            System.out.println("Cold");
+        } else {
+            System.out.println("Normal");
+        }
+        scanner.close();
+    }
 }
