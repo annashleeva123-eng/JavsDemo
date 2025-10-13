9aca1de686654a29532a8f5a6fe642cde1e97b97
AnnaLuneva <annashleeva123@gmail.com>
2025-10-13 17:37:31 +0300

    add homework2.1

--- src/homework1/Homework5.java
+++ src/homework1/Homework5.java
@@ -2,9 +2,11 @@ package homework1;
 
 public class Homework5 {
     public static void main(String[] args) {
-        int q = 21;
+        int q = 29;
         int w = 8;
-        double a = (double) q / w;
+        int a = q / w;
+        int b = q % w;
         System.out.println(a);
+        System.out.println(b);
     }
 }
