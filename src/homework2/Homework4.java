cf6a85279c4a92edec39d47840ab87928d0e2d04
AnnaLuneva <annashleeva123@gmail.com>
2025-10-19 12:29:43 +0300

    add homework2.4

--- src/homework2/Homework4.java
+++ src/homework2/Homework4.java
@@ -1,4 +1,13 @@
 package homework2;
 
 public class Homework4 {
+    public static void main(String[] args) {
+        {
+            int i = 7;
+            while (i <= 98) {
+                System.out.print(i + " ");
+                i += 7;
+            }
+        }
+    }
 }
