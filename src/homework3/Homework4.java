7da0cfc403dc6711581a027ebe8a88065c80cb09
AnnaLuneva <annashleeva123@gmail.com>
2025-10-22 13:45:53 +0300

    add homework3.4

--- src/homework3/Homework4.java
+++ src/homework3/Homework4.java
@@ -1,6 +1,39 @@
 package homework3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Homework4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // Ввод размера массива
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        // Создание массива
+        int[] mass = new int[size];
+
+        // Заполнение случайными числами
+        for (int i = 0; i < size; i++) {
+            mass[i] = random.nextInt(50);
+
+        }
+        int zeroCount = 0;
+        for (int i = 0; i < size; i++) {
+            if (mass[i] == 0) {
+                zeroCount++;
+
+            }
+        }
+            if (zeroCount > 0) {
+                System.out.println("кол-во нулевых элементов " + zeroCount);
+            } else {
+                System.out.println("нет нулевых элементов");
+            }
+        }
+    }
 
 
-}
+
