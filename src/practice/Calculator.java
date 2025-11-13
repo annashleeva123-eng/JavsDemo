3d63bd16b792bb7e8cc5577e3ee58874ecfc8ccc
AnnaLuneva <annashleeva123@gmail.com>
2025-11-13 23:47:51 +0300

    practice

---
+++ src/practice/Calculator.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Calculator {
+
+    double weight;
+    double cost;
+    String color;
+
+    public double addition (double a, double b){
+        return a+b;
+    }
+
+}
